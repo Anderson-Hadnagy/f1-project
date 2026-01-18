501cb8a8a5d5e6e05e23da3168c24a31c3836136
eldobhato50-png <eldobhato5.0@gmail.com>
2026-01-18 17:14:04 +0100

    Update

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,13 @@
 module.exports = function(eleventyConfig) {
-  // This tells Eleventy to copy the admin folder to the output (_site)
+  // This tells Eleventy to copy the admin folder as-is
   eleventyConfig.addPassthroughCopy("admin"); 
   
-  // Optional: If you want to use images from your CMS
-  eleventyConfig.addPassthroughCopy("src/assets/images");
+  // If your files are inside a 'src' folder, use this instead:
+  // eleventyConfig.addPassthroughCopy("src/admin");
 
   return {
     dir: {
-      input: "src", // or wherever your source files live
+      input: "src", // check if your input folder is 'src' or root
       output: "_site"
     }
   };
